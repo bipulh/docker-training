7744383448371860d823bc1f98b342c020c77623
Bipul Haldar <biphalda@cisco.com>
2021-04-01 19:53:23 -0700

    Updated App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! Updated
+          Hello World! From Featured Branch
         </p>
         <a
           className="App-link"
